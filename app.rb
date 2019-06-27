8b76f1b90efdd43677b971926bae0d1de13b2cd3
github username <github email address>
2019-06-27 08:09:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    #retrieves the created new team
+    get '/newteam' do
+        erb :newteam
+    end
+
+    #handles the data and creates a new form && renders or redirects the page to the newly created team.
+    post '/team' do 
+        params[:name]
+        params[:coach]
+        
+        
+        erb :team
+
+        # binding.pry
+    end
 
 end
